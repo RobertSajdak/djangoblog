c53b5114af7c634fe151aeb474c1749f81d558e6
RobertSajdak <robert.sajdak@onet.eu>
2024-01-17 16:21:55 +0100

    New files and html template added.

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # zapis jako 'regex' (wyrażenie regularne): path(r'^admin', admin.site.urls),
+    path('', include('blog.urls')),
 ]
