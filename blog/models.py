6ff99b11dcb6ac987e1e2edc45751dd0eb0792b6
RobertSajdak <robert.sajdak@onet.eu>
2024-01-25 17:59:52 +0100

    Base template added.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -9,7 +10,7 @@ class Post(models.Model):
     publish_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publish_date=timezone.now()
+        self.publish_date = timezone.now()
         self.save()
 
     def __str__(self):
